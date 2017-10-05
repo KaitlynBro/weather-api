6154ef688006c0b9a46da18bda39f5b4fcf6c81c
Kaitlyn Brouwers <kaitlynnicoleb@hotmail.com>
2017-10-05 18:20:15 -0400

    adding data

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,1 +1,67 @@
-console.log('hi')
+let weatherApp = {};
+let api = '13575c20459895df0ea1025d354ec66a';
+
+// http://api.openweathermap.org/data/2.5/weather?APPID=${api_key}&q=${city}
+
+weatherApp.init = () => {
+	weatherApp.getWeather();
+};
+
+
+weatherApp.getWeather = () => {
+	$.ajax({
+		url: `http://api.openweathermap.org/data/2.5/weather?APPID=${api}&q=Toronto`,
+		method: 'GET',
+		dataType: 'json',
+		data: {
+			name: '',
+		}
+	}).then(function(res) {
+		console.log(res);
+		weatherApp.getCity(res);
+	});
+};
+
+
+weatherApp.getCity = function(res) {
+	let temp = JSON.stringify(res.main.temp);
+	let humidity = JSON.stringify(res.main.humidity);
+	let cloudCoverage = JSON.stringify(res.weather[0].main);
+	let numberClouds = JSON.stringify(res.clouds.all);
+	let windSpeed = JSON.stringify(res.wind.speed);
+	let windGusts = JSON.stringify(res.wind.gust);
+	let sunrise = JSON.stringify(res.sys.sunrise);
+	let sunset = JSON.stringify(res.sys.sunset);
+	document.getElementById('weatherInfo').innerHTML += `
+		<div>
+			<p>Current Temperature: ${temp}</p>
+			<p>Cloud Coverage: ${cloudCoverage}</p>
+			<p>humidity: ${humidity}</p>
+			<p>Number of Clouds: ${numberClouds}</p>
+			<p>Wind Speed: ${windSpeed}</p>
+			<p>Wind Gusts: ${windGusts}</p>
+			<p>Sunrise: ${sunrise}</p>
+			<p>Sunset: ${sunset}</p>
+		</div>
+	`;
+};
+
+
+
+
+$(function(){
+	weatherApp.init();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,3 +1,40 @@
 'use strict';
 
-console.log('hi');
+var weatherApp = {};
+var api = '13575c20459895df0ea1025d354ec66a';
+
+// http://api.openweathermap.org/data/2.5/weather?APPID=${api_key}&q=${city}
+
+weatherApp.init = function () {
+	weatherApp.getWeather();
+};
+
+weatherApp.getWeather = function () {
+	$.ajax({
+		url: 'http://api.openweathermap.org/data/2.5/weather?APPID=' + api + '&q=Toronto',
+		method: 'GET',
+		dataType: 'json',
+		data: {
+			name: ''
+		}
+	}).then(function (res) {
+		console.log(res);
+		weatherApp.getCity(res);
+	});
+};
+
+weatherApp.getCity = function (res) {
+	var temp = JSON.stringify(res.main.temp);
+	var humidity = JSON.stringify(res.main.humidity);
+	var cloudCoverage = JSON.stringify(res.weather[0].main);
+	var numberClouds = JSON.stringify(res.clouds.all);
+	var windSpeed = JSON.stringify(res.wind.speed);
+	var windGusts = JSON.stringify(res.wind.gust);
+	var sunrise = JSON.stringify(res.sys.sunrise);
+	var sunset = JSON.stringify(res.sys.sunset);
+	document.getElementById('weatherInfo').innerHTML += '\n\t\t<div>\n\t\t\t<p>Current Temperature: ' + temp + '</p>\n\t\t\t<p>Cloud Coverage: ' + cloudCoverage + '</p>\n\t\t\t<p>humidity: ' + humidity + '</p>\n\t\t\t<p>Number of Clouds: ' + numberClouds + '</p>\n\t\t\t<p>Wind Speed: ' + windSpeed + '</p>\n\t\t\t<p>Wind Gusts: ' + windGusts + '</p>\n\t\t\t<p>Sunrise: ' + sunrise + '</p>\n\t\t\t<p>Sunset: ' + sunset + '</p>\n\t\t</div>\n\t';
+};
+
+$(function () {
+	weatherApp.init();
+});
